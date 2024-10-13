acb85abfc473e52e359952b32deebe50976e44ac
Ali Fırat Kaya <afkaya48300@gmail.com>
2024-10-13 16:59:57 +0300

    router added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './routes/homepage/Homepage.jsx'
+import DashboardPage from './routes/dashboardPage/dashboardPage.jsx'
+import ChatPage from './routes/chatPage/chatPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />
+  },
+  {
+    path: "/dashboard",
+    children: [
+      { path: "/dashboard", element: <DashboardPage /> },
+      { path: "/dashboard/chats/:id", element: <ChatPage /> },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
