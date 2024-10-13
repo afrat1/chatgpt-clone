1cd6f245bb87348997cb1715f4a26b2fbc9672e9
Ali Fırat Kaya <afkaya48300@gmail.com>
2024-10-13 17:34:42 +0300

    layout added

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,30 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Homepage from './routes/homepage/Homepage.jsx'
 import DashboardPage from './routes/dashboardPage/dashboardPage.jsx'
 import ChatPage from './routes/chatPage/chatPage.jsx'
+import RootLayout from './layouts/rootLayout/RootLayout.jsx'
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Homepage />
-  },
-  {
-    path: "/dashboard",
+    element: <RootLayout />,
     children: [
-      { path: "/dashboard", element: <DashboardPage /> },
-      { path: "/dashboard/chats/:id", element: <ChatPage /> },
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />,
+          },
+        ]
+      }
     ]
   },
 ]);
